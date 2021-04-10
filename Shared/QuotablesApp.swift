ac29f7f0d7d2d2f6c8071166433990ed7f5a8c39
Elikem Savie <elviva96@gmail.com>
2021-04-10 16:47:49 +0000

    Initial Commit

---
+++ Shared/QuotablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotablesApp.swift
+//  Shared
+//
+//  Created by Elikem Savie on 10/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuotablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
