d3f52645f00d0aea4a3994e4aa86af6b64050543
Elikem Savie <elviva96@gmail.com>
2021-10-24 21:37:36 +0000

    Embed Home in TabView

---
+++ Shared/Home /MainTabView.swift
@@ -0,0 +1,48 @@
+//
+//  MainTabView.swift
+//  MainTabView
+//
+//  Created by Elikem-OZE on 24/10/2021.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+
+    var body: some View {
+        TabView {
+            HomeView().tabItem {
+                Image(systemName: "house.fill")
+                    .resizable()
+                Text("Home")
+            }
+            HomeView().tabItem {
+                Image(systemName: "scroll")
+                    .resizable()
+                Text("Leaderboard")
+            }
+            HomeView().tabItem {
+                Image(systemName: "plus")
+                    .resizable()
+                Text("Add Quote")
+            }
+            HomeView().tabItem {
+                Image(systemName: "music.mic")
+                    .resizable()
+                Text("Artists")
+            }
+            HomeView().tabItem {
+                Image(systemName: "square.grid.3x3.topleft.filled")
+                    .resizable()
+                Text("Categories")
+            }
+        }.accentColor(.yellow)
+    }
+
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
