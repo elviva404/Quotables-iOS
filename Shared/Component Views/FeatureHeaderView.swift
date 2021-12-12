5f5910b462ce01dfc3d179789f584e7bce9489b0
Elikem Savie <elviva96@gmail.com>
2021-12-12 09:42:14 +0000

    Implement Feature HeaderView

---
+++ Shared/Component Views/FeatureHeaderView.swift
@@ -0,0 +1,34 @@
+//
+//  FeatureHeaderView.swift
+//  Quotables (iOS)
+//
+//  Created by Elikem Savie on 12/12/21.
+//
+
+import SwiftUI
+
+struct FeatureHeaderView: View {
+
+    var body: some View {
+        HStack {
+            Image(systemName: "person.fill")
+                .frame(width: 48, height: 48, alignment: .center)
+                .background(Color.green.opacity(0.5))
+                .clipShape(Circle())
+            VStack(alignment: .leading) {
+                Text("Pseudo Name")
+                Text("Position")
+                    .font(.subheadline)
+                    .foregroundColor(Color("subTitleColor"))
+            }
+            Spacer()
+            Image("verifiedIcon")
+        }
+    }
+}
+
+struct FeatureHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureHeaderView()
+    }
+}
